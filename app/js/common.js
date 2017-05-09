62ddc7384c16389d7d2c5b9d3d0bd40b3e364497
skatt931 <i.kurnytskyi@gmail.com>
2017-05-09 17:38:24 +0300

    add pictures. add some functionality. We can learn but image not changing

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,53 @@
 $(function() {
-	$.getJSON('lessons/lesson1.json', function (data) {
-		let enter = $(".enter");
-		$.each(data, function (key, value) {
-			console.log(value.forin);
-			$(".enter").text(value.mother);
+	const $firstWord = $('.first-word');
+	const $cardImage = $('.card-image');
+	const $checkBtn = $('.check-btn');
+	const $helpBtn = $('.help-btn');
+	let $translation = $('.translation-word');
+
+	function showImage() {
+		$cardImage.attr('src', $firstWord.data('url'));
+	}
+	$.getJSON('lessons/lesson1.json', {scriptCharset: "utf-8"})
+		.done(function (data) {
+			let leng = data.length;
+			let number;
+			let numbersArray = [];
+
+			for(let i = 0; i < leng; i++ ) {
+				number = Math.floor(Math.random() * (leng - 0)) + 0;
+				numbersArray.push(number);
+			}
+
+			//Вивід інформації
+				let k = 0;
+				$firstWord.text(data[numbersArray[k]].forin);
+				$firstWord.attr('data-translation', data[numbersArray[k]].mother);
+				$firstWord.attr('data-url', `img/${data[numbersArray[k]].image}.jpg`);
+				//$cardImage.attr('src', $firstWord.data('url'));
+
+			//console.log($translation.val());
+				$checkBtn.on('click', function(){
+					if ($translation.val() == $firstWord.attr('data-translation')) {
+						k++;
+						$firstWord.text(data[numbersArray[k]].forin);
+						$firstWord.attr('data-translation', data[numbersArray[k]].mother);
+						$firstWord.attr('data-url', `img/${data[numbersArray[k]].image}.jpg`);
+						showImage();
+						$translation.val("");
+					}
+					//if(k < leng) {
+					//	k++;
+					//}
+				});
+			$helpBtn.on('click', showImage);
+
+
+
+				console.log($firstWord.attr('data-url'));
+	})
+		.fail(function(){
+			console.log("fail");
 		});
-	});
+
 });
