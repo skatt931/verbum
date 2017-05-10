faf273a3be1e481adeda09de56868687dbcd6ece
skatt931 <i.kurnytskyi@gmail.com>
2017-05-10 15:50:59 +0300

    add new words for first lesson and add photos to them. use underscore lib for creating new pages with it templates. fix bug of changing photos. add enter button to check the word. fix copy of the words in cards. optimize code.

--- app/js/common.js
+++ app/js/common.js
@@ -1,33 +1,44 @@
 $(function() {
-	const $firstWord = $('.first-word');
-	const $cardImage = $('.card-image');
-	const $checkBtn = $('.check-btn');
-	const $helpBtn = $('.help-btn');
-	let $translation = $('.translation-word');
+	//variables for underscore template
+	let underText = $('.Undertext').html();
+	let underCard = $('.Undercard').html();
+	var textTemplate = _.template(underText);
+	var cardTemplate = _.template(underCard);
 
-	function showImage() {
-		$cardImage.attr('src', $firstWord.data('url'));
-	}
-	$.getJSON('lessons/lesson1.json', {scriptCharset: "utf-8"})
-		.done(function (data) {
-			let leng = data.length;
-			let number;
-			let numbersArray = [];
+    //function for pick datas from json files
+	function pickDataFromServer(){
+		const $firstWord = $('.first-word');
+		const $cardImage = $('.card-image');
+		const $checkBtn = $('.check-btn');
+		const $helpBtn = $('.help-btn');
+		let $translation = $('.translation-word');
 
-			for(let i = 0; i < leng; i++ ) {
-				number = Math.floor(Math.random() * (leng - 0)) + 0;
-				numbersArray.push(number);
-			}
+		function showImage() {
+			$cardImage.attr('src', $firstWord.attr('data-url'));
+			console.log($firstWord.attr('data-url'));
+		}
+		$.getJSON('lessons/lesson1.json', {scriptCharset: "utf-8"})
+			.done(function (data) {
+				let numbersArray = []; //array with numbers of words
 
-			//Вивід інформації
+				data.forEach(function(dat, index) {
+					numbersArray.push(index);
+				});
+
+				function compareRandom(a, b) {
+					return Math.random() - 0.5;
+				}
+				numbersArray.sort(compareRandom);
+				console.log(numbersArray);
+
+
 				let k = 0;
 				$firstWord.text(data[numbersArray[k]].forin);
 				$firstWord.attr('data-translation', data[numbersArray[k]].mother);
 				$firstWord.attr('data-url', `img/${data[numbersArray[k]].image}.jpg`);
 				//$cardImage.attr('src', $firstWord.data('url'));
 
-			//console.log($translation.val());
-				$checkBtn.on('click', function(){
+				function checkWord() {
 					if ($translation.val() == $firstWord.attr('data-translation')) {
 						k++;
 						$firstWord.text(data[numbersArray[k]].forin);
@@ -36,18 +47,30 @@ $(function() {
 						showImage();
 						$translation.val("");
 					}
-					//if(k < leng) {
-					//	k++;
-					//}
+				}
+				$checkBtn.on('click', checkWord);
+				$(document).on('keypress', function(){
+					if(event.keyCode == 13){
+						checkWord();
+					}
 				});
-			$helpBtn.on('click', showImage);
+				$helpBtn.on('click', showImage);
 
 
 
 				console.log($firstWord.attr('data-url'));
-	})
-		.fail(function(){
-			console.log("fail");
-		});
+			})
+			.fail(function(){
+				console.log("fail");
+			});
+
+	}
+
+	$('.forin-mother').on('click',function(){
+		$('.show-content').html(cardTemplate());
+		pickDataFromServer();
+	});
+
+
 
 });
